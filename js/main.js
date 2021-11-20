5f76dd91b78b49e91a4dc1d53da9f1f5f746891e
Akbaraliweb <akbaraliwebdevoloper@gmail.com>
2021-11-20 22:03:45 +0300

    js-2_masala

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var box = [
+    "Jazz, Blues"
+]
+
+box.push(box +" Rock-n-Roll");
+box.push( "Classics," + "Rock-n-Roll");
+box.unshift("Rap,Reggae");
+// box.pop("classics");
+
+console.log(box);
+
+
